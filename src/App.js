9e706bf7ef7b73c4a4cd16faee235a2168a60324
ysnodbs <ysnodabas@gmail.com>
2020-01-17 14:10:08 +0800

    text area fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
- import React from "react"
+ import React, { useState } from "react"
  import './styles.css'
 
 
  function App() {
+  const[text, setText] = useState("")
+
+  function handleChange(e){
+    const{value}=e.target
+    setText(value)
+
+  }
+
    return (
      <div>
        <h1>Speed Typing Game</h1>
-       <textarea/>
-       <h4>Time Remaining</h4>
+       <textarea
+        value ={text}
+        onChange ={handleChange}
+       />
+       <h4>Time Remaining:</h4>
        <button>Start Game</button>
-       <h1>Word Count</h1>
+       <h1>Word Count:</h1>
      </div>
    )
  }
