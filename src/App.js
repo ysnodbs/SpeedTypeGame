3794f69c7d5b9731ac78a7ec9db8c84d367ad4da
ysnodbs <ysnodabas@gmail.com>
2020-01-17 13:48:19 +0800

    first part of the game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+ import React from "react"
+ import './styles.css'
 
-function App() {
-  return (
-  <h1> </h1>
-  );
-}
 
-export default App;
+ function App() {
+   return (
+     <div>
+       <h1>Speed Typing Game</h1>
+       <textarea/>
+       <h4>Time Remaining</h4>
+       <button>Start Game</button>
+       <h1>Word Count</h1>
+     </div>
+   )
+ }
+
+ export default App;
