5a3c08e3bc6a795e5a166ea6d0f0978185a927d0
ysnodbs <ysnodabas@gmail.com>
2020-01-18 20:54:41 +0800

    starting time fixed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
- import React, { useState, useEffect } from "react"
+ import React, { useState, useEffect, useRef } from "react"
  import './styles.css'
 
 
  function App() {
+  const STARTING_TIME = 5
   const[text, setText] = useState("")
-  const[timeRemaining, setTimerRemaining] = useState(5)
+  const[timeRemaining, setTimerRemaining] = useState(STARTING_TIME)
   const[isGameRunning, setIsGameRunning] = useState(false)
   const[wordCount, setWordCount] = useState(0)
+  const textAreaRef =useRef(null)
    
   //Control text area when input comes
   function handleChange(e){
     const{value}=e.target
     setText(value)
   }
-  //Timer is set up here so it won't go below 0
   
   //Calculate how many words have been typed
   function calculateWordCount(text){
     var wordCount = text.trim().split(" ")
     return wordCount.filter(word => word!=="").length
   }
+
+  //Start of the game, start timer , set text area to empty text and focus on text area
   function startGame(){
     setIsGameRunning(true)
-    setTimerRemaining(5)
+    setTimerRemaining(STARTING_TIME)
     setText("")
+    textAreaRef.current.disabled=false
+    textAreaRef.current.focus()
   }
 
   function endGame(){
@@ -31,7 +36,7 @@
       setWordCount(calculateWordCount(text))
 
   }
-  
+  //Timer is set up here so it won't go below 0
   useEffect(() => {
     if (isGameRunning && timeRemaining > 0) {
       setTimeout(() => setTimerRemaining(prevState => prevState - 1), 1000)
@@ -44,6 +49,7 @@
      <div>
        <h1>Speed Typing Game</h1>
        <textarea
+        ref={textAreaRef}
         value ={text}
         onChange ={handleChange}
         disabled={!isGameRunning}
